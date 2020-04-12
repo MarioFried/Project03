cf1ea234b1fe3e8342d22eea156e37647c8cd045
Mario Fried <mario.fried@gmail.com>
2020-04-12 14:57:20 +0300

    Aprendendo Estrutura Dicionario

---
+++ Dicionario.py
@@ -0,0 +1,10 @@
+"""
+Pequeno Exemplo de Funcionamento da Estrutura Dicionario em Python
+"""
+Database={}
+Database['Cliente']={1:'Mario',2:'Ana'}
+Database['Leads']={1:'Manu',2:'David',3:'Marcelo'}
+
+print(Database['Cliente'][1])
+print(Database['Leads'][3])
+print(Database)
